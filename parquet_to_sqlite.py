ebf80f2430fd01fc6d151d3bb97299d313c7c693
Mike <dalupus@gmail.com>
2018-08-12 13:36:24 -0700

    add ability to skip creating indexes

--- parquet_to_sqlite.py
+++ parquet_to_sqlite.py
@@ -5,22 +5,23 @@ import sqlite3
 import logging
 
 
-def convert_table(source, table, conn):
+def convert_table(source, table, conn, skip_indexes):
     logging.warning(f'Reading {table}')
     df = pd.read_parquet(os.path.join(source, table))
     logging.warning(f'Writing {table}')
     df.to_sql(table, conn, index=False, if_exists='replace', index_label=[f'{table}_id'])
-    logging.warning(f'Creating indexs for {table}')
-    id_cols = [col for col in df.columns.tolist() if col.endswith('_id')]
-    for col in id_cols:
-        conn.execute(f'CREATE INDEX {table}_{col}_index on {table}({col})')
+    if not skip_indexes:
+        logging.warning(f'Creating indexes for {table}')
+        id_cols = [col for col in df.columns.tolist() if col.endswith('_id')]
+        for col in id_cols:
+            conn.execute(f'CREATE INDEX {table}_{col}_index on {table}({col})')
 
 
-def parquet_to_sqlite(source, destination, db_name):
+def parquet_to_sqlite(source, destination, db_name, skip_indexes):
     tables = [name for name in os.listdir(source) if os.path.isdir(os.path.join(source, name))]
     conn = sqlite3.connect(os.path.join(destination, db_name)+'.sqlite')
     for table in sorted(tables):
-        convert_table(source, table, conn)
+        convert_table(source, table, conn, skip_indexes)
 
 
 def main():
@@ -30,7 +31,7 @@ def main():
     db_name = opts.name
     if not db_name:
         db_name = os.path.basename(source)
-    parquet_to_sqlite(source, destination, db_name)
+    parquet_to_sqlite(source, destination, db_name, opts.skip_indexes)
 
 
 def parse_arguments():
@@ -38,6 +39,7 @@ def parse_arguments():
     parser.add_argument("source", help="Path to directory containing the parquet files")
     parser.add_argument("destination", help="Path to the location where you want to write the databse")
     parser.add_argument("--name", help="What to name the database.  Default is it uses the directory name pointed to")
+    parser.add_argument("--skip-indexes", help="Set this to bypass creating indexes", action='store_true')
     return parser.parse_args()
 
 
